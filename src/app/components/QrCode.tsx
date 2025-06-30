d050badb8b4a8138de75f7cd0f12974fe38d3f93
sophiacheong <sophiaacheong@gmail.com>
2025-06-30 23:13:18 +0900

    addd url

--- src/app/components/QrCode.tsx
+++ src/app/components/QrCode.tsx
@@ -1,11 +1,20 @@
 "use client";
 import { QRCodeCanvas } from "qrcode.react";
+import { useEffect, useState } from "react";
 
 export const QRPage = () => {
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setUrl(`${window.location.origin}/form`);
+    }
+  }, []);
+
   return (
     <div className="flex flex-col items-center mt-20">
       <h1 className="text-2xl font-bold mb-4">Scan to Enter</h1>
-      <QRCodeCanvas value="http://localhost:3000/form" size={256} />
+      <QRCodeCanvas value={`${url}/form`} size={256} />
     </div>
   );
 };
